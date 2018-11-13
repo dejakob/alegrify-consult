6b80df0195ee8132057a56b0010f935154e600f2
Alegrify <happy@alegrify.com>
2018-11-13 20:08:36 +0000

    update logo

---
+++ release-2018_11_13__20_07_24/precache-manifest.60dcdc3cbaf9c265e03695985b567c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62f7b2f87f152dc2375c",
+    "url": "/static/js/main.62f7b2f8.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "62f7b2f87f152dc2375c",
+    "url": "/static/css/main.209f1e3c.chunk.css"
+  },
+  {
+    "revision": "3d303431fa5d5d0d4bcf471984ecd0cf",
+    "url": "/index.html"
+  }
+];
