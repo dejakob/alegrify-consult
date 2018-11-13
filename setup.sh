fef1c8910cc02d631affc78e90f7770f8f75496c
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2018-11-13 20:07:28 +0100

    server config

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ sudo apt-get update
 sudo apt-get install nginx
 sudo apt-get install git
 # Make sure ~/.ssh/*.pub is added to github
+git config --global user.email happy@alegrify.
+git config --global user.name Alegrify
 git clone git@github.com:dejakob/alegrify-consult.git
 ./alegrify-consult/deploy.sh
