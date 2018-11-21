849b65bf35cc05e02a11ba098873ee4b3e05ba56
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2018-11-21 22:25:25 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ git clean -fd
 git pull
 rm -rf node_modules
 npm i
+npm i dejakob/alegrify-ui
+npm i dejakob/react-alegrify-ui
 npm run build
 version=`date '+%Y_%m_%d__%H_%M_%S'`;
 cp -R ./build ./release-$version
