2eef7de19707d0b0230038c53d69ead2429b3a23
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-05-01 11:47:12 +0200

    nginx config

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,9 @@ git reset --hard
 git clean -fd
 git pull
 rm -rf node_modules
-npm i
-npm i dejakob/alegrify-ui
-npm i dejakob/react-alegrify-ui
-npm run build
-version=`date '+%Y_%m_%d__%H_%M_%S'`;
-sudo cp -R ./build ./release-$version
-sudo cp -R ./build/* /var/www/html && sudo chown -R www-data:www-data /var/www/html
+yarn
+yarn build
+sudo chown -R www-data:www-data ./build/*
+sudo cp -R ./build/* /var/www/html
 rm -rf ./build
-/etc/init.d/nginx restart
+cp ./config/nginx_default /etc/nginx/sites-available/default
